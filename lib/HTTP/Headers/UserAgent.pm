f52009bdcc1d6228260cbb762589f439d9c849b1
Olaf Alders <olaf@wundersolutions.com>
2013-08-14 23:23:29 -0400

    HTTP::BrowserDetect has not been deprecated ;)

--- lib/HTTP/Headers/UserAgent.pm
+++ lib/HTTP/Headers/UserAgent.pm
@@ -47,7 +47,7 @@ HTTP::Headers::UserAgent - identify browser by parsing User-Agent string (deprec
 
 =head1 DESCRIPTION
 
-This module, HTTP::BrowserDetect, is deprecated. I suggest you use one
+This module, HTTP::Headers::UserAgent, is deprecated. I suggest you use one
 of the other modules which provide the same functionality. Check the
 SEE ALSO section for pointers. This module is being kept on CPAN for
 the moment, in case someone is using it, but at some point in the future
